c2f98685cc272081a23c9f3fc6bf7556195ccf0d
hjy628 <hjy628@gmail.com>
2017-09-25 16:39:20 +0800

    demo

---
+++ src/main/java/com/hjy/mapper/CustomerMapper.java
@@ -0,0 +1,16 @@
+package com.hjy.mapper;
+
+import com.hjy.domain.Customer;
+import java.util.List;
+
+public interface CustomerMapper {
+    int deleteByPrimaryKey(Long id);
+
+    int insert(Customer record);
+
+    Customer selectByPrimaryKey(Long id);
+
+    List<Customer> selectAll();
+
+    int updateByPrimaryKey(Customer record);
+}
---
+++ src/main/java/com/hjy/mapper/Empty.java
@@ -0,0 +1,7 @@
+package com.hjy.mapper;
+
+/**
+ * Created by hjy on 17-9-25.
+ */
+public interface Empty {
+}
